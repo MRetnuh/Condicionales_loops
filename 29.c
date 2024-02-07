b79eba87c2ce5b3cfe8d581fb99c04abfed07588
MRetnuh <130599861+MRetnuh@users.noreply.github.com>
2024-02-07 20:30:22 -0300

    Create 29.c

---
+++ 29.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+double harmonic (int n) {
+double sum = 0.0;
+  for (int i = 1; i <= n; i++){
+    sum += 1.0/i;
+  }
+  return sum;
+}
+int main(int argc, char *argv[]){
+  int n = atoi(argv[1]);
+double h = harmonic(n);
+printf("El numeto harmonico %d es %f\n", n, h);
+return 0;
+}
