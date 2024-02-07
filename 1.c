bd16e4ba5054fc2987f3699829b60f9ea8698c38
MRetnuh <130599861+MRetnuh@users.noreply.github.com>
2024-02-07 20:33:58 -0300

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[]){
+  int n = atoi(argv[1]);
+  int i = 0;
+  while(i<n){
+    puts("Hola");
+      i++;
+  }
+  return 0;
+}
