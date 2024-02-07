e8afadd9743b27a6398e259f017c01af7f23e45f
MRetnuh <130599861+MRetnuh@users.noreply.github.com>
2024-02-07 20:22:46 -0300

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[]){
+  int n = atoi(argv[1]);
+  int c = 0;
+  double suma = 0.0;
+  while (c > n){
+    c++;
+    int r = rand() % 100;
+    printf("%d", r);
+    suma += r;
+  }
+  printf("\n promedio: %f\n", suma / n);
+  return 0;
+}
